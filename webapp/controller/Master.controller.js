9001a93fbfda180b77d8464f6af2c0f4ffd10cdd
Grzegorz Malczewski <malczewski.gm@gmail.com>
2019-08-22 13:52:37 +0000

    after step 6

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function (Controller, formatter) {
+	"../model/formatter",
+	"sap/ui/unified/ColorPickerPopover"
+], function (Controller, formatter, ColorPickerPopover) {
 	"use strict";
 
 	return Controller.extend("deepdive.DeepDive.controller.Master", {
@@ -15,6 +16,24 @@ sap.ui.define([
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("Detail", {
 				soId: oEvent.getSource().getBindingContextPath().substr(1)
+			});
+		},
+
+		onApplyColorPress: function (oEvent) {
+			var oColorPickerSimplifiedPopover = new ColorPickerPopover({
+				colorString: "lightgray",
+				displayMode: sap.ui.unified.ColorPickerDisplayMode.Simplified,
+				mode: sap.ui.unified.ColorPickerMode.HSL,
+				change: [this._colorSelected, this]
+			});
+			oColorPickerSimplifiedPopover.openBy(oEvent.getSource());
+		},
+
+		_colorSelected: function (oEvent) {
+			var sColor = oEvent.getParameter("colorString");
+			var iNumber = this.getView().byId("everyNStepInput").getValue();
+			$("#" + this.getView().byId("masterTable").getId() + " > table > tbody > tr:nth-child(" + iNumber + "n)").css({
+				"background-color": sColor
 			});
 		}
 
